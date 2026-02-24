4c078080ffd53bcd899a26f0fde6373ec0af2f98
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-02-24 16:01:28 +0200

    Lesson 2, Basic variable syntax
    Learned the basic syntax for variables

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var nameOne string = "John"
+	var nameTwo = "Doe"
+	var nameThree string
+	nameFour := "Smith"
+
+	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	var ageOne int = 30
+	var ageTwo = 25
+	var ageThree int
+	ageFour := 40
+	fmt.Println(ageOne, ageTwo, ageThree, ageFour)
+
+	//bits and memory
+	var bigNumber int8 = 52
+	var smallNumber int8 = -128
+	var num int16 = 32767
+	var mediumNumber uint8 = 25
+	fmt.Println(bigNumber, smallNumber, num, mediumNumber)
+
+	var scoreOne float32 = 95.5
+	var scoreTwo float64 = 89.75
+	scoreThree := 78.25
+
+	println(scoreOne, scoreTwo, scoreThree)
 }
