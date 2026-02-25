afd819b06ee12d4531c4fa4a3fe8856859a9b5c8
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-02-25 19:17:18 +0200

    Lesson 11, maps
    Learned the syntax for maps.

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 func main() {
-	SayHello("James")
-
-	for _, value := range points {
-		fmt.Println(value)
-
+	menu := map[string]float64{
+		"soup":          4.99,
+		"pie":           7.99,
+		"salad":         6.99,
+		"toffe pudding": 3.55,
 	}
-	ShowScore()
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	//looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+
+	phonebook := map[int]string{
+		2326: "Luigi",
+		6523: "Mario",
+		9999: "Emergency",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[9999])
+
+	phonebook[2326] = "bowser"
+
+	fmt.Println(phonebook[2326])
 }
