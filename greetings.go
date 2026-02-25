40ad466777b3959d940bb841dd999ed51028039d
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-02-25 18:53:18 +0200

    Lesson 10, Using variables in multiple files
    Learned how to use variables and functions in multiple files

---
+++ greetings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 40, 70}
+
+func SayHello(name string) {
+	fmt.Println("hello", name)
+}
+
+func ShowScore() {
+	fmt.Println("score:", score)
+}
--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func GetInitials(name string) (string, string, string, string) {
-	s := strings.ToUpper(name)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, value := range names {
-		initials = append(initials, value[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1], initials[2], initials[3]
-	}
-
-	return initials[0], "_", "_", "_"
-}
+var score = 99.5
 
 func main() {
-	firstName, secondName, thirdName, fourthName := GetInitials("John Edgar Allen Doe")
+	SayHello("James")
 
-	fmt.Println(firstName, secondName, thirdName, fourthName)
+	for _, value := range points {
+		fmt.Println(value)
+
+	}
+	ShowScore()
 }
