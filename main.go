64aa1b60eda53d8c58f123d2cca0c3fd992649b3
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-02-26 13:25:52 +0200

    Lesson 13, Struct syntax
    Learned the syntax for structs

--- main.go
+++ main.go
@@ -2,19 +2,8 @@ package main
 
 import "fmt"
 
-func UpdateName(x *string) {
-	*x = "wedge"
-}
-
 func main() {
-	name := "tifa"
+	myBill := newBill("mario's bill")
 
-	m := &name
-
-	fmt.Println("memory address:", m)
-	fmt.Println("value at memory adress:", *m)
-
-	fmt.Println(name)
-	UpdateName(m)
-	fmt.Println(name)
+	fmt.Println(myBill)
 }
